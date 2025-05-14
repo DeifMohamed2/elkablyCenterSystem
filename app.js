aaad831ebdda1ef4426c3b33ea217d8f664480fe
deifecommerce <deifecommerce@gmail.com>
2025-05-14 21:58:22 +0300

    update mongo

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.json());
 // const dbURI = 'mongodb://localhost:27017/ElkablyCenter';
 
 
-const dbURI ='mongodb+srv://3devWay:1qaz2wsx@cluster0.5orkagp.mongodb.net/elkablyCenter?retryWrites=true&w=majority&appName=Cluster0';
+const dbURI ='mongodb+srv://deif:1qaz2wsx@3devway.aa4i6ga.mongodb.net/elkablyCenter?retryWrites=true&w=majority&appName=Cluster0';
 mongoose
   .connect(dbURI)
   .then((result) => {
